47746cf43752cced3e6acef60e010cd0e6ad1df1
RizafiDev <firmansyah.riza6@smk.belajar.id>
2025-07-10 10:01:03 +0700

    add: developers

---
+++ database/migrations/2025_07_10_025916_create_developers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('developers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('logo', 255)->nullable();
+            $table->text('description')->nullable();
+            $table->text('address')->nullable();
+            $table->string('phone', 20)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('website', 255)->nullable();
+            $table->integer('established_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('developers');
+    }
+};
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="">
+        <h1>tess</h1>
+    </div>
+</x-app-layout>
